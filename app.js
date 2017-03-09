96b67699bc0014c5f5d794a5f6552a62b46f284e
Reagan Wood <reaganwood1@gmail.com>
2017-03-09 14:21:16 -0600

    added code from class, lots of errors

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 "use strict";
 
-
+var fs = require('fs');
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('scrumtastic.sqlite3', function(err) {
   if(err) console.error(err);
 });
 
+var router = new (require('./lib/migrate').Router);
+
+router.add('/', function(req, res) {
+  fs.readFile('/public/index.html', function(err, body){
+    res.end(body);
+  });
+});
+
 var migrate = require('./lib/migrate');
 migrate(db, 'migrations', function(err){
-  db.serialize(function(){
-    db.run("INSERT INTO projects (name) values (?)", ['title']);
-    db.all("SELECT * FROM projects", [], function(err, rows){
-      if(err) return console.error(err);
-      console.log("ROWS:", rows);
-    });
+  var server = new http.Server(function(req, res){
+
+    router.toute(req, res);
   });
+  server.listen(3000, function(){
+    console.log("listening on port " + 3000);
+  });
+
 });
